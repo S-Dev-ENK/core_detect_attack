a9456c7a04be29c9d49f56c2ea89c56014b6df85
doneni <xxerrenn@icloud.com>
2024-11-17 16:44:05 +0900

    feat: add prprocessing url

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,17 @@
-def 
+from urllib.parse import urlparse
 
-def preprocessing_domains(domain_list):
+def preprocessing_domains(domain: str) -> str:
+    parsed_url = urlparse(domain)
+    domain_name = parsed_url.netloc or parsed_url.path
+    
+    if domain_name.startswith("www."):
+        domain_name = domain_name[4:]
+    
+    if ':' in domain_name:
+        domain_name = domain_name.split(':')[0]
+    
+    return domain_name
+
+def test():
+    domain = "http://asdf.asdf.sdaf.as-dsd.com"
+    print(preprocessing_domains(domain))
